2d34c006306054c143400011d9e9a8cdb22b5445
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-11-04 01:08:12 +0100

    update package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,15 @@ import PackageDescription
 let package = Package(
     name: "DependencyInjector",
     products: [
-        .library(
-            name: "DependencyInjector",
-            targets: ["DependencyInjector"])
-    ],
-    dependencies: [
+        .library(name: "DependencyInjector", targets: ["DependencyInjector"])
     ],
     targets: [
-        .target(
-            name: "DependencyInjector",
-            dependencies: [
-            ]),
+        .target(name: "DependencyInjector"),
         .testTarget(
             name: "DependencyInjectorTests",
-            dependencies: ["DependencyInjector"])
+            dependencies: [
+                "DependencyInjector"
+            ]
+        )
     ]
 )
